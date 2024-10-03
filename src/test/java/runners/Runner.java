c109b7f488e7c29f40f0f05fd636c958114d912a
MustafaZengin32 <120048898+MustafaZengin32@users.noreply.github.com>
2024-10-03 17:46:23 +0200

    second

---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,27 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        plugin = {"json:target/cucumber.json",
+                "html:target/cucumber-default-report",
+                "rerun:target/failed.txt"},
+
+        features = "src/test/resources/UI_Features" ,
+        glue = "src/test/java/stepdefinitions",
+        tags = "@us000",
+        dryRun = false
+)
+
+
+
+public class Runner {
+
+
+
+
+}
---
+++ src/test/java/utilities/Authentication.java
@@ -0,0 +1,36 @@
+package utilities;
+
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+public class Authentication {
+
+    public static String tokenGenerate(){
+
+        String url = "https://medunna.com/api/authenticate";
+
+
+        Map<String, String> expectedData = new HashMap();
+        expectedData.put("username","infoTech02");
+        expectedData.put("password","Admin123.");
+
+
+        Response response = given().contentType(ContentType.JSON).body(expectedData).when().post(url);
+
+
+        return response.path("id_token");
+
+
+    }
+
+    public  static void main(String[] args){System.out.println(tokenGenerate());
+    }
+
+
+}
